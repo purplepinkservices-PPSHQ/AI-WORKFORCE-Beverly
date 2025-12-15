a1d5112c481fbffebe99c9813e066e67ccce5c4b
Larry Adler <purplepinkservices@gmail.com>
2025-12-15 17:20:51 +0100

    fix: notion client v5 compatible import

--- src/notion/client.js
+++ src/notion/client.js
@@ -1,9 +1,15 @@
+// src/notion/client.js
+
 const { Client } = require("@notionhq/client");
 
 let notionInstance = null;
 
 function getNotion() {
     if (notionInstance) return notionInstance;
+
+    if (!process.env.NOTION_API_KEY) {
+        throw new Error("NOTION_API_KEY fehlt in den ENV-Variablen");
+    }
 
     notionInstance = new Client({
         auth: process.env.NOTION_API_KEY,
